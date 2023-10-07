c3ec9d40e6c1e65134b04aaa8ba7d834bfa343dc
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-07 07:57:30 +0000

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+/*
+ * this is a a function 
+ * that always returns a 0
+ */
+
 int main(void)
 {
-/*
- * This is the main funtion 
- * and it returns an int
- */
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
