57c0ee27e77de51088cd9e0732df2285350beeed
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-07 07:58:50 +0000

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /*
  * this is a a function 
  * that always returns a 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
