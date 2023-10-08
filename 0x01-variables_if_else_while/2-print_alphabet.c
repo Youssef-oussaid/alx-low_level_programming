4b96ecec8c8bff65d87f52e87576b57d4e885f7b
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-08 10:25:28 +0000

    printing alphabets with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char alph = 97;
+
+	while (alph <= 122)
+	{
+		putchar(alph);
+		alph++;
+	}
+	return (0);
+}
