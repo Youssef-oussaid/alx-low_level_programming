7f59474d1ca25e54e84fbc68180f55104b17288f
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-09 19:39:49 +0000

    displaying numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%i\n", num);
+		num++;
+	}
+	return (0);
+}
