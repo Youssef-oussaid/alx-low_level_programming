dc0be80160d2aabbe1e34bd3712dadfd87f9444e
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-11 16:16:12 +0000

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,13 +4,13 @@
  * _islower - finds if the character is lower case
  *
  * Description: a
- * 
+ *
  * Returns 0
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <=122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
