b72a5aa41ca08e62fb47c7914c2955c8035ff942
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-11 15:33:29 +0000

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: a
+ *
+ * Alphabets a to z
+ */
+
+void print_alphabet(void)
+
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
