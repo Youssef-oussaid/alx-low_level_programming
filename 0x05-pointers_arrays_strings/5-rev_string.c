1035fe99d107037362f2181eeac9a93e01cb36d9
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-16 21:21:11 +0000

    prints a string in reverse my school

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - to reverse a string
+ *
+ * @s: stores a string
+ *
+ * return: always 0
+ */
+
+void rev_string(char *s)
+{
+	for (i = 9; i >= 0; i--)
+	{
+		_putchar(*s[i]);
+	}
+	_putchar('\n');
+}
