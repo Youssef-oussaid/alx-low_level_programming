76592835996e60c7609572e91949bac2748cf108
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-16 21:43:01 +0000

    prints a string in reverse my school

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,18 @@
 
 void rev_string(char *s)
 {
-	int i;
+	int length = 0;
+	int i, j;
 
-	for (i = 9; i >= 0; i--)
+	while (s[length] != '\0')
 	{
-		_putchar(s[i]);
+		length++;
 	}
-	_putchar('\n');
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		char temp = s[i];
+
+		s[i] = s[j];
+		s[j] = s[temp];
+	}
 }
