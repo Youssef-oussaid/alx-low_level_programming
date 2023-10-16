dfa28a28af16320d1119a35cce6829f5b8cd6025
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-16 20:49:44 +0000

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: stores a string
+ *
+ * return: always 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n);
+}
