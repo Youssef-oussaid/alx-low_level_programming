580b037ca2dbd0ee0c18cf074a17a0acde863dc3
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-17 22:51:18 +0000

    a

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	while (*src != '\0')
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
-	*dest = '\0';
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
 	return (dest);
 }
