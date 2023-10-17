078d6f4b06c1237b9b47d09f711207a52a64700d
ousssaid Youssef <ousssaid-youssef@outlook.com>
2023-10-17 21:13:17 +0000

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_array - prints the content of an array
+ *
+ * @a: holds an int
+ *
+ * @n: holds the number of indexes
+ *
+ * return: always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i; 
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		]
+	}
+}
