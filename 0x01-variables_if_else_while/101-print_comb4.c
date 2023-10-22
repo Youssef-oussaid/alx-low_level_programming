f205c2e71797fd9a527125a4b119cca345547951
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-22 09:42:57 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -26,7 +26,7 @@ int main(void)
 					putchar(digit1 + 48);
 					putchar(digit2 + 48);
 					putchar(digit3 + 48);
-					if (digit1 + digit 2 + digit3 != 24)
+					if (digit1 + digit2 + digit3 != 24)
 					{
 						putchar(',');
 						putchar(' ');
