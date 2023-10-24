b13403772a5171bfafcbb81f711317dbcd486e72
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-24 12:16:13 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - function
+ * @haystack: an array
+ * @needle: an array
+ * Return: a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *p = NULL;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (needle[j] == haystack[i] &&
+			needle[j + 1] == haystack[i + 1] &&
+			needle[j + 2] == haystack[i + 2])
+			{
+				p = &haystack[i];
+				return (p);
+			}
+		j++;
+		}
+	i++;
+	}
+	return (p);
+}
