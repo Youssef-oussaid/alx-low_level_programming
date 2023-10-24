a1bbe542f4e8f4f7f3e70a6aa00b4a282ebf3031
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-24 04:25:07 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strchr - searches for a char.
+ * @s: a string.
+ * @c: a character.
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+}
