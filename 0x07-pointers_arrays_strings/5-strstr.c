57140d4cfa02c8882d0b6d97e55d6222f1bf1fe9
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-24 12:31:31 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,27 +9,17 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-	char *p = NULL;
-
-	i = 0;
-	while (haystack[i] != '\0')
+	for (;*haystack != '\0'; haystack++)
 	{
-		j = 0;
-		while (needle[j] != '\0')
+		while (*haystack == *needle)
 		{
-			if (needle[j] == haystack[i] &&
-			needle[j + 1] == haystack[i + 1] &&
-			needle[j + 2] == haystack[i + 2] &&
-			needle[j + 3] == haystack[i + 3] &&
-			needle[j + 4] == haystack[i + 4])
-			{
-				p = &haystack[i];
-				return (p);
-			}
-		j++;
+			haystack++;
+			needle++;
 		}
-	i++;
+		if (*needle == '\0')
+		{
+			return (haystack);
+		}
 	}
-	return (p);
+	return (NULL);
 }
