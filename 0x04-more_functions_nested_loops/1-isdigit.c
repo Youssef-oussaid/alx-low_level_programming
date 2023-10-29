56a21ec2c14077bd7b511625364cb74614031fdf
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-29 02:49:22 -0700

    a

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - function
+ * @c: char
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else if (c >= 48 && c <= 57)
+	{
+		return (0);
+	}
+	return (0);
+}
