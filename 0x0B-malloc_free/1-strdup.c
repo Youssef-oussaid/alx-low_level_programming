f430406cdfa61da5a199274c7097680a1519e87e
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-31 15:05:01 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - a function to duplicate a string
+ * @str: a string
+ * Return: a pointer
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	m = malloc(size * sizeof(char) + 1);
+
+	if (m == 0)
+		return (NULL);
+
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
