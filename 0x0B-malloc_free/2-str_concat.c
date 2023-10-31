53557efdb1f2e4701faedeb71cbf4b01081810c3
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-31 15:56:44 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,12 @@ char *str_concat(char *s1, char *s2)
 	int size1 = 0, size2 = 0;
 	char *n;
 
+	if (s1 == NULL)
+		s1 = "\0";
+
+	if (s2 == NULL)
+		s2 = "\0";
+
 	while (s1[size1] != '\0')
 		size1++;
 
