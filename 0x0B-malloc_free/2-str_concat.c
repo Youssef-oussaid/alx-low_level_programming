a014cc3ed27bc60099fed47a08a1137893eca082
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-10-31 15:49:35 -0700

    a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * str_concat - a function that concatenates strings
+ * @s1: string one
+ * @s2: string two
+ * Return: a pointer to a string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0;
+	char *n;
+
+	while (s1[size1] != '\0')
+		size1++;
+
+	while (s2[size2] != '\0')
+		size2++;
+
+	n = malloc((size1 + size2) * sizeof(char));
+
+	if (n == NULL)
+		return (NULL);
+
+	else
+	{
+		int i;
+
+		for (i = 0; i < size2; i++)
+		{
+			int j;
+
+			for (j = 0; j < size1; j++)
+			{
+				n[j] = s1[j];
+			}
+		n[i + j] = s2[i];
+		}
+	}
+	return (n);
+}
