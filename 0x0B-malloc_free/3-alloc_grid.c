3e4ed321130422f69705c11fd44688a1dcc382bb
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-01 07:52:57 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 {
 	int **tab, i, j;
 
-	tab = malloc(sizeof(**tab) * height);
+	tab = malloc(sizeof(*tab) * height);
 
 	if (width <= 0 || height <= 0 || tab == 0)
 	{
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	{
 		for (i = 0; i < height; i++)
 		{
-			tab[i] = malloc(sizeof(**tab) * width);
+			tab[i] = malloc(sizeof(*tab) * width);
 			if (tab[i] == 0)
 			{
 				while (i--)
