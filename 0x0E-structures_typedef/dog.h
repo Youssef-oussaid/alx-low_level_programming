8463f8f4ea9788c6623c63d6fdfbd777427b4e46
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-05 10:26:22 -0800

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure
+ * @name: the name
+ * @age: an float
+ * @owner: a name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
