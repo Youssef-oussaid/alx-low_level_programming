970a35c591f797cf587400f915a4a11c136ae30c
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-05 11:15:34 -0800

    a

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,9 +11,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d;
-
-	dog.d->name = *name;
-	dog.d->age = *age;
-	dog.d->owner = *owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
