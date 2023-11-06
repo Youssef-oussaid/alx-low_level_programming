fbf1067291f40ecae66c546f955252aca812881f
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-06 14:32:30 -0800

    a

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - a function
+ * @d: a struct pointer
+ * Return: always 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit(0);
+	else
+	{
+
+		if (d.name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d.name);
+
+		if (d.age == NULL;
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d.age);
+
+		if (d.owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d.owner);
+	}
+}
