562a9f9b1f7eb6954cb1523b5436e03f25df7b95
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-06 15:19:26 -0800

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,9 +19,9 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if ((*d).age == 0.0)
+		/*if ((*d).age == 0.0)
 			printf("Age: (nil)\n");
-		else
+		else*/
 			printf("Age: %f\n", d->age);
 
 		if ((*d).owner == NULL)
