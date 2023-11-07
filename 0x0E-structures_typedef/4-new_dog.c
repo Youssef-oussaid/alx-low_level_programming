8c44c67dcfdb926fa95bb9b4d9155acd957a7cdb
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-07 11:44:56 -0800

    a

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - a function
+ * @name: a string
+ * @age: a float
+ * @owner: a string
+ * Return: a pointer to a struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newdog == NULL)
+		return (NULL);
+
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
+
+	if (newdog->name == NULL || newdog->owner == NULL)
+	{
+		free(newdog->name);
+		free(newdog->owner);
+		free(newdog);
+		return (NULL);
+	}
+	return (newdog);
+}
