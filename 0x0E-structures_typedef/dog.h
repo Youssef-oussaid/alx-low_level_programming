0637365b6d0390c46865d5c37602479f9672e432
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-07 13:22:30 -0800

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,14 @@
  * @owner: a name
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
