58c14a320b219c4f6a02376e71bc24b47399646d
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-07 16:18:04 -0800

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - a function
+ * @int: integer
+ * Return: an int
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+		num = -num;
+	if (num >= 0)
+		num = num;
+	return(num);
+}
