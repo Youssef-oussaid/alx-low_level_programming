716d31c052e149857c867ea12af89357366f583a
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-09 15:45:12 -0800

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -66,11 +66,11 @@ void print_all(const char * const format, ...)
 	while (format && format[i])
 	{
 		j = 0;
-		while (tokens[j].token)
+		while (token[j].token)
 		{
-			if (format[i] == tokens[j].token[0])
+			if (format[i] == token[j].token[0])
 			{
-				tokens[j].f(separator, ap);
+				token[j].f(separator, ap);
 				separator = ", ";
 			}
 			j++;
