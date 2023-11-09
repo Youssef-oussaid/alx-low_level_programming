023c79ebbf7500902a36264c1a8d7778dd6cf1a7
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-09 14:46:32 -0800

    a

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function
+ * @separator: a char
+ * @n: numberof args
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	if (n > 0)
+	{
+		printf("%d", va_arg(args, int);
+		for (i = 1; i < n; i++)
+		{
+			if (separator == NULL)
+			{
+				printf("%d", va_arg(args, int);
+			}
+			else
+				print("%s%d", separator, va_arg(args, int);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
