331a42a6b2efdd16129045e4584adbb870582873
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-09 14:49:19 -0800

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
 	if (n > 0)
 	{
 		printf("%d", va_arg(args, int);
