8964dc2d1583e17c598d4526f534986228dcfd94
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-10 14:07:54 -0800

    a

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
