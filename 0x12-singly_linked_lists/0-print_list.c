f99b72f769d7883f46c779ce4fc5e02fe9208c64
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-21 11:11:26 -0800

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the content of a list
+ * @h: a pointer to a struct of type list_t
+ * Return: 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	list_t *temp = *h;
+	
+	if (temp->str == NULL)
+	{
+		printf("[0] (nil)");
+		exit(0);
+	}
+
+	while (temp != NULL)
+	{
+		printf("[%d] %s\n", temp->len, temp->str);
+		count++;
+	}
+	return (count);
+}
