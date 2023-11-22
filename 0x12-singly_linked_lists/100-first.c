51f911b04a43458c17bb112b0435f2943af7ad0f
TheJocker <oussaid-youssef@outlook.com>
2023-11-22 21:00:40 +0100

    a

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_first - a function that prints before main
+ * Return: void
+*/
+
+void __attribute__((constructor)) print_first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
