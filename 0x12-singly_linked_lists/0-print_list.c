eb96772db241d19423018503f23bef832788f507
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-22 08:22:46 -0800

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -23,13 +23,13 @@ int _strlen(char *s)
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
-		count++;
+		i++;
 	}
-	return (count);
+	return (i);
 }
