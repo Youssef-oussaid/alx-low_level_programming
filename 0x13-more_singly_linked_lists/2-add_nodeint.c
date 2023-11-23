a30ad1913869980f822d8d1c83097d667540c21f
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-23 12:41:05 +0100

    a

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a node
+ * @head: a pointer to a struct
+ * @n: the value of the new node
+ * Return: pointer or NULL
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *new_node = malloc(sizeof(listint_t));
+    listint_t *nodePtr = NULL;
+
+    if (new_node == NULL || nodePtr == NULL)
+        return (NULL);
+
+    if (!head || !n)
+        return (NULL);
+    nodePtr = *head;
+    new_node->n = n;
+    new_node->next = nodePtr;
+    *head = new_node;
+    return (new_node);
+}
