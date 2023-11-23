d7534c6fb47c60badaeebd3f8c42c191b957f5fe
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-23 12:58:15 +0100

    a

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a node to the end of the a list
+ * @head: a ptr to a pointer of a struct
+ * @n: a value to store in the new node
+ * Return: a pointer to the new node or NULL
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    
+}
