10c335605a49e0fc234e500b8c5c496e6e02d1f5
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-23 12:11:11 +0100

    a

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the nodes
+ * @h: a pointer to a struct
+ * Return the num of nodes
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->n;
+		i++;
+	}
+	return (i);
+}
