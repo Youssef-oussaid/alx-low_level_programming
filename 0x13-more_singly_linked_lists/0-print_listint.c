92c54883269e7548cd4e5890d90909426a2d4e43
TheJocker <oussaid-youssef@outlook.com>
2023-11-23 11:37:18 +0100

    a

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the values of a list
+ * @h: a pointer to a list
+ * 
+ * Return: the number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
