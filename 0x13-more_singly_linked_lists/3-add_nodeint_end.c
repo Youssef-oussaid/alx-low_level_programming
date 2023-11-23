314ec3123fe0a311420c049dd2a1f4e212ece0fd
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-23 14:27:38 +0100

    a

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,5 +9,26 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    
+    listint_t *new_node = malloc(sizeof(listint_t));
+    listint_t *nodePtr = NULL;
+
+    if (new_node == NULL)
+        return (NULL);
+
+    if (!*head || !n)
+        return (NULL);
+
+    new_node->n = n;
+    new_node->next = NULL;
+
+    nodePtr = *head;
+    while (nodePtr->next != NULL)
+    {
+        nodePtr = nodePtr->next;
+    }
+    nodePtr->next = new_node;
+
+    if (*head == NULL)
+        *head = new_node;
+    return (new_node);
 }
