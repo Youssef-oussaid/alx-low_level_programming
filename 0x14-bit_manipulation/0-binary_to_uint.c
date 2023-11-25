7b8d03c260c30a4e8adc4774a04b5e7a2aab71b3
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-25 12:29:20 +0100

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (int i = 0; i <= stringLen - 1; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (total);
 	}
 
