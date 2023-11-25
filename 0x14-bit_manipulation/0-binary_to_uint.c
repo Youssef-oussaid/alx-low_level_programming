cdb607d92e421362e1bd0b7c239f30200e27de68
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-25 12:15:02 +0100

    a

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - a function
+ * @b: a string of numbers
+ * Return: the decimal value
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int total = 0;
+    unsigned int decval = 1;
+    unsigned stringLen = strlen(b);
+
+    for (int i = stringLen - 1; i >= 0; i--)
+    {
+        if (b[i] != 0)
+        {
+            total += decval;
+        }
+        decval *= 2;
+    }
+    return (total);
+}
