f46e07bd80da1c92183ef436aaee6f16c56e0bbe
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-25 12:26:38 +0100

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,26 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int total = 0;
-    unsigned int decval = 1;
-    unsigned stringLen = strlen(b);
+	unsigned int total = 0;
+	unsigned int decval = 1;
+	unsigned stringLen = strlen(b);
 
-    for (int i = 0; i <= stringLen - 1; i++)
-    {
-        if (b[i] != 0 || b[i] != 1 || b == NULL)
-            return (total);
-    }
+	if (b == NULL)
+		return (total);
 
-    for (int i = stringLen - 1; i >= 0; i--)
-    {
-        if (b[i] != 0)
-        {
-            total += decval;
-        }
-        decval *= 2;
-    }
-    return (total);
+	for (int i = 0; i <= stringLen - 1; i++)
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (total);
+	}
+
+	for (int i = stringLen - 1; i >= 0; i--)
+	{
+		if (b[i] != 0)
+		{
+			total += decval;
+		}
+		decval *= 2;
+	}
+	return (total);
 }
