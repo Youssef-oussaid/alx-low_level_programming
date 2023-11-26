830b4d39b62195fc4e40709c28d53daf635494ce
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-26 23:13:56 +0100

    a

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - a function that returns the value of a certain bit
  * @n: a number
  * @index: the position of the bit
- * 
+ *
  * Return: the value of the bit
 */
 
@@ -14,9 +14,6 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (!n)
 		return (-1);
-	else
-	{
-		value = (n>>index) & 1;
-		return (value);
-	}
+	value = (n >> index) & 1;
+	return (value);
 }
