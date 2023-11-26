78ad3be59207647479d7b6d487e92b1acce9647e
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-11-27 00:10:17 +0100

    a

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(n) * 8)
-        return (-1);
-    return (!!(*n &= 0L << index));
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+	return (1);
 }
