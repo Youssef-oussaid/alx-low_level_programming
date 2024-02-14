0bd920e9de80c7d77f64d62b37172b23ff13e0ac
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-14 16:12:08 +0100

    a

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlintint_len - length
+ * @h: address of head
+ * Return: size
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
