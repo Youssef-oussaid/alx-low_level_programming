c8a160b0a72b5cd35f5dd875de5c499ec3689869
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-17 05:23:35 +0100

    a

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds node
+ * @head: address
+ * @n: int
+ * Return: Address
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *add = malloc(sizeof(dlistint_t));
+
+	if (!head || !new)
+		return (new ? free(new), NULL : NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	if (!*head)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
+	return (new);
+}
