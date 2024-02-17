00fe05d7ea17ef82dd2bfaa27d468db9650a87f8
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-17 05:35:12 +0100

    a

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,16 +18,16 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!*head)
 	{
 		new->prev = NULL;
-        *head = new;
+		*head = new;
 	}
 	else
 	{
 		node = *head;
 		while (node->next)
-            node = node->next;
+			node = node->next;
 
-        node->next = new;
-        new->prev = node;
-    }
+		node->next = new;
+		new->prev = node;
+	}
 	return (new);
 }
