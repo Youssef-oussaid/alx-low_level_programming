5fff6d18c1db9c0cfba6f222b8552b7ce981efeb
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-02 08:24:44 +0100

    a

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include <hash_tables.h>
+
+/**
+ * key_index - gives the index of key
+ * @key: key to get the index for
+ * Return: index for the key
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    return (hash_djb2(key) % size);
+}
