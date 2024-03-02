7950bc5ae4a38dc7b5a8276de7906449d68013c1
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-02 08:41:34 +0100

    a

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,6 +3,7 @@
 /**
  * key_index - gives the index of key
  * @key: key to get the index for
+ * @size: size of the table
  * Return: index for the key
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
